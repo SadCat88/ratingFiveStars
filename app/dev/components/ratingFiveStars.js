87b04266622fc9e2b16517415cd4953a93015cee
SadCat88 <SadCat88@yandex.ru>
2019-12-27 18:57:28 +0300

    Улучшенная логика поведения.

--- app/dev/components/ratingFiveStars.js
+++ app/dev/components/ratingFiveStars.js
@@ -34,6 +34,22 @@ const setRating = function(event) {
 
 		displayStar($inputRating);
 	}
+
+	// === если событие - наведение мышью
+	if (event.type == 'mousemove') {
+		// === если наведение на звезду звездам
+		if ($target.classList.contains('star') == true) {
+			let $onHoverNumberStar = event.target.dataset.numStar;
+			// на какую звезду наведение
+
+			displayStar($onHoverNumberStar, 'hover');
+		}
+	}
+
+	// === если событие - покидание мышью границ блока
+	if (event.type == 'mouseleave') {
+		displayStar(0, 'leave');
+	}
 };
 
 /**
@@ -43,10 +59,11 @@ const setRating = function(event) {
  * Принимает числовое значение рейтинга.
  * На основе чего закрашивает количество звезд равное рейтингу.
  * Затем очищает остальные звезды (на случай изменения рейтинга пользователем).
+ * Также при наведении на звезду подкрашивает предыдущие звезды.
  * </pre>
  * @param {number} rating - рейтинг от 1 до 5
  */
-const displayStar = rating => {
+const displayStar = (rating, hover) => {
 	$allyInput.value = rating;
 	// записать в input значение равное номеру звезды
 
@@ -54,12 +71,31 @@ const displayStar = rating => {
 	for (let i = 1; i <= 5; i++) {
 		let $star = document.querySelector(`.star[data-num-star="${i}"]`);
 
-		if (i <= rating) {
-			// установка класса .star_fill на все звезды до клика
-			$star.classList.add('star_fill');
-		} else {
-			// удаление класса .star_fill со всех звезд после клика
-			$star.classList.remove('star_fill');
+		// === если событие не является наведением мыши ========================
+		if (hover == undefined) {
+			if (i <= rating) {
+				// установка класса .star_fill на звезды до клика включительно
+				$star.classList.add('star_fill');
+			} else {
+				// удаление класса .star_fill со всех звезд после клика
+				$star.classList.remove('star_fill');
+			}
+		}
+
+		// === если событием является наведением мыши ==========================
+		if (hover == 'hover') {
+			if (i < rating) {
+				// установка класса .star_hover на звезды до клика
+				$star.classList.add('star_hover');
+			} else {
+				$star.classList.remove('star_hover');
+			}
+		}
+
+		// === если событием является покидание мышью родительского блока ======
+		if (hover == 'leave') {
+			// удаление класса .star_hover со всех звезд
+			$star.classList.remove('star_hover');
 		}
 	}
 };
--- app/public/pages/index/index.js
+++ app/public/pages/index/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import { setRating } from '../../components/ratingFiveStars.js';
-let $starClick = document.querySelector('.rating-5-stars');
+let $starImages = document.querySelector('.rating-5-stars');
 let $starInput = document.querySelector('.ally-rating__input');
-$starClick.addEventListener('click', setRating);
+$starImages.addEventListener('click', setRating);
+$starImages.addEventListener('mousemove', setRating);
+$starImages.addEventListener('mouseleave', setRating);
 $starInput.addEventListener('input', setRating);
 
 console.log('index.js is run');
